eadd2a289894fc5548e6fb3a44074f2b9850a14b
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-24 09:58:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const table=document.getElementById("sampleTable");
+ 
 function insert_Row() {
-    //Write your code here
-  
-  
+	const tableRow=document.createElement("tr");
+	const tableData1=document.createElement("td");
+	tableData1.append("New Cell1");
+	const tableData2=document.createElement("td");
+	tableData2.append("New Cell2");
+	tableRow.appendChild(tableData1);
+	tableRow.appendChild(tableData2);
+	table.appendChild(tableRow)
 }
